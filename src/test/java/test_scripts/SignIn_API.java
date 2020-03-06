26d973ff05d1c48600cedebbc19780b79dda9d65
Surbhi Rajput <srajput@dminc.com>
2020-03-06 16:20:04 +0530

    Listener

--- src/test/java/test_scripts/SignIn_API.java
+++ src/test/java/test_scripts/SignIn_API.java
@@ -1,42 +1,53 @@
 package test_scripts;
 
-import org.json.simple.JSONObject;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import spreadsheet.ReadSpreadSheet;
 
 public class SignIn_API {
 
-	@SuppressWarnings("unchecked")
+	ReadSpreadSheet readSpreadSheet;
+	List<List<Object>> TestData;
+
+	public SignIn_API() {
+		readSpreadSheet=new ReadSpreadSheet();
+		try {
+			TestData=readSpreadSheet.readSpreadSheet("TestData");
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
 	@Test
 	public void SignIn() {
 
 		//Specify base URI
 		RestAssured.baseURI = "http://stlukesuat.azurewebsites.net";
-
+		
 		//Request Object
 		RequestSpecification httpRequest = RestAssured.given();
 
-		//Payload Sending
-		JSONObject requestParams = new JSONObject();
-
-		requestParams.put("password", "Password1");
-		requestParams.put("email", "ladybug@grr.la");
-
-		//Adding headers and parameters
-		httpRequest.header("Content-Type","application/json");
-		httpRequest.header("ApiAccessKey","o1mgpGUMtweufnz2pirg2Yc6NR6hxYwJZtI9ucsqRGhQY7BOrDayKta4It9fGdZU");
-		httpRequest.body(requestParams.toJSONString());
-
+		//Adding headers
+		  httpRequest.header("Content-Type","application/json");
+		  httpRequest.header("ApiAccessKey",
+		  "o1mgpGUMtweufnz2pirg2Yc6NR6hxYwJZtI9ucsqRGhQY7BOrDayKta4It9fGdZU");
+		 
+		  //Passing request body
+		httpRequest.body(TestData.get(1).get(5).toString());
+		
 		//Response
 		Response response=httpRequest.request(Method.POST, "/api/v3/user/signin");
-		
+
 		System.out.println(response.getStatusCode());
-		
-		System.out.println(response.jsonPath().get("access_Token"));
+
+		//System.out.println(response.jsonPath().get("access_Token"));
 
 		System.out.println(response.getBody().asString());
 	}
